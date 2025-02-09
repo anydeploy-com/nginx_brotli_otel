903d718eee6fd00159dc2ed7d590c1e055e1155b
Martin Plocinski <martin@anydeploy.com>
2025-02-09 23:36:54 +0000

    x

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ cd ngx_brotli/deps/brotli
 mkdir out && cd out
 cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DCMAKE_C_FLAGS="-Ofast -m64 -march=native -mtune=native -flto -funroll-loops -ffunction-sections -fdata-sections -Wl,--gc-sections" -DCMAKE_CXX_FLAGS="-Ofast -m64 -march=native -mtune=native -flto -funroll-loops -ffunction-sections -fdata-sections -Wl,--gc-sections" -DCMAKE_INSTALL_PREFIX=./installed ..
 cmake --build . --config Release --target brotlienc
-cd ../../..
+cd ../../../..
 
 
 # Build Nginx
